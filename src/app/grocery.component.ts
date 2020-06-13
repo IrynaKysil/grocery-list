b8c6ab99eb52d6855927faef0016d49b6a4beffc
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-06-12 21:23:36 -0700

    marking the grocery item as done

--- src/app/grocery.component.ts
+++ src/app/grocery.component.ts
@@ -17,14 +17,15 @@ export class GroceryComponent {
 
   onClick(){
     if(this.task.id == 0){
-      this.tasks.push({id: (new Date()).getTime(),name: this.task.name});
+        this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false});
+    }
 
-      this.task = {
+    this.task = {
         name: '',
         id: 0
-      };
-    }
-}
+    };
+  }
+
     onEdit(item){
       this.task = item;
   }
@@ -36,7 +37,19 @@ export class GroceryComponent {
             break;
         }
     }
-}
+   }
 
-
+   onStrike(item){
+    for(var i = 0;i < this.tasks.length; i++){
+      if(item.id == this.tasks[i].id){
+        if(this.tasks[i].strike){
+          this.tasks[i].strike = false;
+        }
+        else{
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
 }
